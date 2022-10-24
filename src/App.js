a09f7bfb80b6f39a245dc7b6d4041e8786f506f0
879n <33409937+879n@users.noreply.github.com>
2022-10-24 16:34:29 +0900

    日本語を追記

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-//            Edit <code>src/App.js</code> and save to reload.
-            hoge hoge
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            日本語表示テスト
           </p>
           <a
             className="App-link"
